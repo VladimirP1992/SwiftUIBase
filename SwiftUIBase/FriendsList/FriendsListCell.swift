32d3825d3e912ed8dd23a2bd366ebad0796caafd
Vladimir <p.vladimir.m@yandex.ru>
2021-02-05 23:53:51 +0300

    Classes for Controllers and Cells

---
+++ SwiftUIBase/FriendsList/FriendsListCell.swift
@@ -0,0 +1,23 @@
+//
+//  FriendsListCell.swift
+//  SwiftUIBase
+//
+//  Created by Владимир Поливников on 05.02.2021.
+//
+
+import UIKit
+
+class FriendsListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

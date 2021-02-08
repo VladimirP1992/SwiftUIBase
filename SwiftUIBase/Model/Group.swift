e99261cd651c8596a17e683eeb01ea86b4662bff
Vladimir <p.vladimir.m@yandex.ru>
2021-02-08 20:17:12 +0300

    User and Group models

---
+++ SwiftUIBase/Model/Group.swift
@@ -0,0 +1,13 @@
+//
+//  Group.swift
+//  SwiftUIBase
+//
+//  Created by Владимир Поливников on 08.02.2021.
+//
+
+import Foundation
+
+struct Group {
+    var name: String
+    var subscribersCount: Int
+}
---
+++ SwiftUIBase/Model/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  SwiftUIBase
+//
+//  Created by Владимир Поливников on 08.02.2021.
+//
+
+import Foundation
+
+struct User {
+    var id: String
+    var name: String
+    var surname: String
+    var avatarName: String
+}

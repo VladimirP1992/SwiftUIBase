f308544111544d6bc724437e19bb07e4df46c658
Vladimir <p.vladimir.m@yandex.ru>
2021-02-13 15:27:32 +0300

    AvatarView completed

--- SwiftUIBase/MyView/AvatarView.swift
+++ SwiftUIBase/MyView/AvatarView.swift
@@ -5,9 +5,32 @@
 //  Created by Владимир Поливников on 09.02.2021.
 //
 
-import Foundation
 import UIKit
 
+@IBDesignable
 class AvatarView: UIView {
+    
+    @IBOutlet var imageView: UIImageView! {
+        didSet {
+            self.imageView.layer.cornerRadius = cornerRadius
+            self.imageView.layer.masksToBounds = true
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+//            self.imageView.layer.cornerRadius = cornerRadius
+//            self.imageView.layer.masksToBounds = true
+        }
+    }
+    
+    @IBInspectable var shadowOffset: CGSize = .zero {
+        didSet {
+            self.layer.shadowColor = UIColor.black.cgColor
+            self.layer.shadowOpacity = 0.3
+            self.layer.shadowOffset = shadowOffset
+        }
+    }
     
 }

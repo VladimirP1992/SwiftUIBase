8416bce4a205355ff2528160a070b7c380d11c50
Vladimir <p.vladimir.m@yandex.ru>
2021-02-15 22:38:00 +0300

    Header for friends list

---
+++ SwiftUIBase/FriendsList/FriendsListSectionHeader.swift
@@ -0,0 +1,17 @@
+//
+//  FrientsListSectionHeader.swift
+//  SwiftUIBase
+//
+//  Created by Владимир Поливников on 15.02.2021.
+//
+
+import UIKit
+
+final class FriendsListSectionHeader: UITableViewHeaderFooterView {
+    
+    @IBOutlet weak var firstLetterOfSurnameLabel: UILabel!
+    
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}

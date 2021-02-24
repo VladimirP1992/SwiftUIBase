09cb3bc2c7f681cf42629038855544e4e192d317
Vladimir <p.vladimir.m@yandex.ru>
2021-02-24 21:20:29 +0300

    News TableViewController created

---
+++ SwiftUIBase/News/NewsTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NewsTableViewController.swift
+//  SwiftUIBase
+//
+//  Created by Владимир Поливников on 24.02.2021.
+//
+
+import UIKit
+
+class NewsTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
